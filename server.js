8b76e5203b0ed2466d408d9a4b45f81c052a1d31
Naoufel Bellali <naoufel.bellali@external.total.com>
2020-09-06 21:24:21 +0200

    test

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/angular-heroku'));
+app.use(express.static('./dist/cat-mash'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/cat-mash/'}),
 );
 
 app.listen(process.env.PORT || 8080);
